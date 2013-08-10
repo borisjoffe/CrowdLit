3c1c2aaeb1271fc699de37a9c1cbc263891061b9
Boris Joffe <boris@joff3.com>
2013-08-10 14:13:55 -0700

    Initial commit

---
+++ api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+
+from flask import Flask, render_template, url_for, request, redirect, session, g
+app = Flask(__name__)
+ 
+app.debug = False
+brand = "CrowdList"
+
+@app.route('/')
+def index():
+	return render_template('home.html', brand=brand)
+
+@app.route('/home')
+def index():
+	return render_template('userpage.html', brand=brand)
+
+
+def main():
+    app.run()
+    return 0
+
+if __name__ == "__main__":
+	sys.exit(main())
+
