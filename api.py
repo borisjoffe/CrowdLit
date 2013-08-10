25624c2c1827af017b60c0119fbde033f90c565b
Boris Joffe <boris@joff3.com>
2013-08-10 16:39:14 -0700

    port 4000

--- api.py
+++ api.py
@@ -17,7 +17,8 @@ def home():
 	return render_template('userpage.html')
 
 def main():
-	port = int(os.environ.get('PORT', 5000))	# for heroku
+	port = 5000
+	#port = int(os.environ.get('PORT', 5000))	# for heroku
 	app.run(host='0.0.0.0', port=port)
 	return 0
 
